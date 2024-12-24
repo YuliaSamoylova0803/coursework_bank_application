8c30f430b7880eafa8e79735085db30853aad15d
Yulia Samoylova <u.u.samoylova@gmail.com>
2024-12-24 22:44:53 +0500

    add func spending by category

---
+++ src/reports.py
@@ -0,0 +1,54 @@
+from functools import wraps
+from typing import Any, Callable, Optional
+import datetime
+import datetime as dt
+from src.utils import get_excel_dataframe, get_date
+
+
+def log(filename: Optional[str] = None) -> Callable:
+    """Декоратор, который может логировать работу функции и ее результат как в файл, так и в консоль."""
+
+    def logging_decorator(func: Any) -> Any:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} ok\n")
+                else:
+                    print(f"{func.__name__} ok")
+                return result
+
+            except Exception as error:
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} error: {error.__class__.__name__}. Inputs: {args}, {kwargs}\n")
+                else:
+                    print(f"{func.__name__} error: {error.__class__.__name__}. Inputs: {args}, {kwargs}")
+
+        return wrapper
+
+    return logging_decorator
+
+
+def spending_by_category(df_transactions: pd.DataFrame, category: str, date: [str] = None) -> pd.DataFrame:
+    """Функция возвращает траты по заданной категории за последние три месяца (от переданной даты)"""
+    if date is None:
+        fin_date = dt.datetime.now()
+    else:
+        fin_date = get_date(date)
+    start_date = fin_date.replace(hour=0, minute=0, second=0, microsecond=0) - datetime.timedelta(days=91)
+    transactions_by_category = df_transactions.loc[
+        (pd.to_datetime(df_transactions["Дата операции"], dayfirst=True) <= fin_date)
+        & (pd.to_datetime(df_transactions["Дата операции"], dayfirst=True) >= start_date)
+        & (df_transactions["Категория"] == category)
+    ]
+    return  transactions_by_category
+
+
+if __name__ == "__main__":
+    result = spending_by_category(
+        get_excel_dataframe(abs_src_file_path1), "Аптеки", "26.07.2019 20:58:55"
+    )
+    print(result)
