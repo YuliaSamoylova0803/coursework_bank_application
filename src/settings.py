7f8067ce4cd40447e8b8e55d89eaa31a5a04e770
Yulia Samoylova <u.u.samoylova@gmail.com>
2024-12-24 23:12:36 +0500

    add file setting path before files

---
+++ src/settings.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+excel_filename = Path(BASE_DIR, "data", "operations.xlsx")
+reports_log = Path(BASE_DIR, "logs", "reports_file.txt")
+project_log = Path(BASE_DIR, "logs", "logs_file.txt")
