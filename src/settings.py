52f09b3a782cbb941c5414c88593a8188deb74d7
Yulia Samoylova <u.u.samoylova@gmail.com>
2024-12-25 14:39:22 +0500

    update file settings

--- src/settings.py
+++ src/settings.py
@@ -7,3 +7,4 @@ BASE_DIR = Path(__file__).parent.parent
 excel_filename = Path(BASE_DIR, "data", "operations.xlsx")
 reports_log = Path(BASE_DIR, "logs", "reports_file.txt")
 project_log = Path(BASE_DIR, "logs", "logs_file.txt")
+stock_rates_path = Path(BASE_DIR, "src", "user_settings.json")
