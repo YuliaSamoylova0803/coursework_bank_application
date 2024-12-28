d78dde949cdaa10a3cbad0f42b22847be4056f0e
Yulia Samoylova <u.u.samoylova@gmail.com>
2024-12-28 21:45:27 +0500

    update file main

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from logging import INFO, FileHandler, StreamHandler, basicConfig, getLogger
 from pathlib import Path
 
 from src.settings import BASE_DIR
-from src.utils import get_excel_dataframe, get_expenses_cards, top_transactions
-from src.views import get_currency_exchange_rates, get_greeting, get_stock_prices
 
 excel_filename = Path(BASE_DIR, "data", "operations.xlsx")
 reports_log = Path(BASE_DIR, "logs", "reports_file.txt")
@@ -25,31 +23,40 @@ basicConfig(level=INFO, format=FORMAT, handlers=[file_handler, console])
 
 
 # Входящая дата
-date_now = "2018-02-16 12:01:58"
+# date_now = "29.07.2019 22:06:27"
 
 
-def main():
-    """Функция принимает на вход DataFrame и возвращает json-файл"""
-    logger.info("Начало работы функции main")
-    greeting = get_greeting(date_now)
-    cards = get_expenses_cards(get_excel_dataframe(excel_filename))
-    top_trans = top_transactions(get_excel_dataframe(excel_filename))
-    stocks_prices = get_stock_prices(stock_rates_path)
-    currency_rates = get_currency_exchange_rates(stock_rates_path)
-    logger.info("Формирование JSON ответа")
-    result_list_dicts = {
-        "greeting": greeting,
-        "cards": cards,
-        "top_transactions": top_trans,
-        "currency_rates": currency_rates,
-        "stock_prices": stocks_prices,
-    }
+# def main():
+#     """Функция принимает на вход DataFrame и возвращает json-файл"""
+#     logger.info("Начало работы функции main")
+#     greeting = get_greeting(date_now)
+#     cards = get_expenses_cards(get_excel_dataframe(excel_filename))
+#     top_trans = top_transactions(get_excel_dataframe(excel_filename))
+#     stocks_prices = get_stock_prices(stock_rates_path)
+#     currency_rates = get_currency_exchange_rates(stock_rates_path)
+#     logger.info("Формирование JSON ответа")
+#     result_list_dicts = {
+#         "greeting": greeting,
+#         "cards": cards,
+#         "top_transactions": top_trans,
+#         "currency_rates": currency_rates,
+#         "stock_prices": stocks_prices,
+#     }
+#
+#     # Формируем json-ответ
+#     logger.info("json-ответ создан успешно")
+#     json_output = json.dumps(result_list_dicts, ensure_ascii=False, indent=4)
+#     return json_output
+#
+#
 
-    # Формируем json-ответ
-    logger.info("json-ответ создан успешно")
-    json_output = json.dumps(result_list_dicts, ensure_ascii=False, indent=4)
-    return json_output
-
-
-if __name__ == "__main__":
-    print(main())
+# if __name__ == "__main__":
+#     df_transactions = get_excel_dataframe(excel_filename)
+#     date_now = "29.07.2019 22:06:27"
+#
+#
+#
+#     date_json = main(df_transactions, date, currency_rates, stocks_prices)
+#
+#     print(date_json)
+#     print(excel_filename)
